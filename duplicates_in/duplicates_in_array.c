37ea6815acc1f0b8c066eb6cbb6e36a6a4ffbc23
Harishanker S Nair <110891240+14-Hari-14@users.noreply.github.com>
2023-10-16 12:01:50 +0530

    Update and rename labcycle3q2.c to duplicates_in/duplicates_in_array.c
    added comments explaining the code

--- duplicates_in/duplicates_in_array.c
+++ duplicates_in/duplicates_in_array.c
@@ -2,18 +2,19 @@
 #include<stdio.h>
 void main()
 {
+	//initializing variables
 	int i,n,dupe,c=0,j=0;
 	printf("Enter the length of the array: ");
 	scanf("%d", &n);
 	int arr[n];
-	
+	//entering elements into the array
 	for(i=0;i<n;i++)
 	{
 		printf("Enter the element: ");
 		scanf("%d", &arr[i]);
 	}
 	
-	
+	//checking for duplicate by storing a value in the dupe variable
 	for(j=0;j<n;j++)
 	{
 		for(i=0;i<n;i++)
